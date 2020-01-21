b225409c1be8552abfefc91c9123238448cdf5c6
Marco Perdomo <marcooperdomo@gmail.com>
2020-01-21 17:14:31 -0600

    Rutas con nombre

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,21 @@ Route::get('/hola/{nombre}', function ($nombre) {
 
 /* Ejemplo - Ruta con parámetros opcionales */
 Route::get('/hola/{nombre?}', function ($nombre = "Juan") {
-    return "Hola mundo $nombre";
+    /* Link sin Rutas con nombre */
+        // return "Hola $nombre conocenos: <a href='/nosotros'>Nosotros</a>";
+    /* Link con Rutas con nombre */
+        return "Hola $nombre conocenos: <a href='". route('nosotros') . "'>Nosotros</a>";
 });
+
+/* Ejemplo - Ruta con nombre */
+Route::get('/sobre-nosotros', function () {
+    return "<h1>Toda la información sobre nosotros!</h1>";
+})->name("nosotros");
 
 /* Notas:
     *Si se tienen rutas iguales, el orden de lectura de laravel es de arriba hacía abajo, por lo que la ruta de abajo será
      la usada y mostrada por laravel
+    *Las rutas con nombre nos permite cambiar la url sin necesidad de cambiar el href de todos los enlaces del HTML 
+     sea uno o mil
+        *Se usa el helper route('nameRoute') de laravel
 */
